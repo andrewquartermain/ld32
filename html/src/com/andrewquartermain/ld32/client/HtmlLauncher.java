ad7d44cb1f75213adf67f1f9603cd7a5d89b3301
Andrew Quartermain <ananew@gmail.com>
2015-04-18 06:25:28 +0100

    Added libGdx project core, desktop, android and html5.

---
+++ html/src/com/andrewquartermain/ld32/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.andrewquartermain.ld32.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.andrewquartermain.ld32.LD32;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new LD32();
+        }
+}
